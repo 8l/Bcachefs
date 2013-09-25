be646fb741b4ac289b9359296be3ddfe00567761
Kent Overstreet <kmo@daterainc.com>
2013-09-25 13:38:25 -0700

    Introduce blk_max_segment()
    Signed-off-by: Kent Overstreet <kmo@daterainc.com>

--- drivers/xen/biomerge.c
+++ drivers/xen/biomerge.c
@@ -3,6 +3,14 @@
 #include <linux/export.h>
 #include <xen/page.h>
 
+bool xen_page_phys_mergeable(const struct page *p1, const struct page *p2)
+{
+	unsigned long mfn1 = pfn_to_mfn(page_to_pfn(p1));
+	unsigned long mfn2 = pfn_to_mfn(page_to_pfn(p2));
+
+	return mfn1 + 1 == mfn2;
+}
+
 bool xen_biovec_phys_mergeable(const struct bio_vec *vec1,
 			       const struct bio_vec *vec2)
 {

462cded4515ad04c489e06d53fc8668f866bae31
Nicholas Swenson <nks@daterainc.com>
2013-11-14 14:26:10 -0800

    bcache: Generational garbage collection
    Generational grouping is a technique that attempts to keep
    hot and cold data separate. It essentially just groups
    data based on age, to keep hot and cold data together. Previous
    patches have sorted the data we are compacting into genertaions,
    stored in GC_GEN. This patch does the actual writing to different
    buckets.
    
    To allow for generation sorting, moving_gc needed its own sector
    allocation, as the standard allocator sticks data sticks new writes
    into whatever bucket pops out of the lru. Moving gc_alloc_sectors
    is new function created to handle this allocation. It will take
    the GC_GEN, and allocate sectors in the appropriate bucket for that
    get. Each cache keeps an array of gc_open_buckets to allocate
    sectors from.
    
    To mark the use of the gc_sector allocator, data_insert_op received
    a new field moving_gc to denote such a write.
    
    note: removed write_prio from bch_alloc_seectors because it was
          only use to distinguish gc writes from others. This is not
          needed because gc_writes use a different allocator.
    
    note: this doesn't support multiple cache devices. It will only move
          ptr[0] and ignore the rest.
    
    Signed-off-by: Nicholas Swenson <nks@daterainc.com>
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -546,7 +546,7 @@ void bch_prio_write(struct cache *ca)
 		for (b = ca->buckets + i * prios_per_bucket(ca);
 		     b < ca->buckets + ca->sb.nbuckets && d < end;
 		     b++, d++) {
-			d->prio = cpu_to_le16(b->prio);
+			d->prio = cpu_to_le16(b->read_prio);
 			d->gen = b->gen;
 		}
 
@@ -618,7 +618,7 @@ static void prio_read(struct cache *ca, uint64_t bucket)
 			d = p->data;
 		}
 
-		b->prio = le16_to_cpu(d->prio);
+		b->read_prio = le16_to_cpu(d->prio);
 		b->gen = b->last_gc = d->gen;
 	}
 }
@@ -1830,6 +1830,7 @@ void bch_cache_release(struct kobject *kobj)
 static int cache_alloc(struct cache_sb *sb, struct cache *ca)
 {
 	size_t free;
+	unsigned movinggc_reserve;
 
 	__module_get(THIS_MODULE);
 	kobject_init(&ca->kobj, &bch_cache_ktype);
@@ -1838,14 +1839,18 @@ static int cache_alloc(struct cache_sb *sb, struct cache *ca)
 	ca->journal.bio.bi_max_vecs = 8;
 	ca->journal.bio.bi_io_vec = ca->journal.bio.bi_inline_vecs;
 
+	movinggc_reserve = max_t(size_t, NUM_GC_GENS * 2,
+				 ca->sb.nbuckets >> 7);
+
 	free = max_t(size_t, 4, ca->sb.nbuckets >> 10);
 
 	if (!init_fifo(&ca->free[RESERVE_BTREE], 8, GFP_KERNEL) ||
 	    !init_fifo(&ca->free[RESERVE_PRIO], prio_buckets(ca), GFP_KERNEL) ||
-	    !init_fifo(&ca->free[RESERVE_MOVINGGC], free, GFP_KERNEL) ||
+	    !init_fifo(&ca->free[RESERVE_MOVINGGC],
+		       movinggc_reserve, GFP_KERNEL) ||
 	    !init_fifo(&ca->free[RESERVE_NONE], free, GFP_KERNEL) ||
 	    !init_fifo(&ca->free_inc,	free << 2, GFP_KERNEL) ||
-	    !init_heap(&ca->heap,	free << 3, GFP_KERNEL) ||
+	    !init_heap(&ca->heap,	movinggc_reserve << 1, GFP_KERNEL) ||
 	    !(ca->buckets	= vzalloc(sizeof(struct bucket) *
 					  ca->sb.nbuckets)) ||
 	    !(ca->prio_buckets	= kzalloc(sizeof(uint64_t) * prio_buckets(ca) *

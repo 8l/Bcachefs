0d13cdd99adba8d4eed16bc59d0a27e0e5abd1f8
Slava Pestov <sp@daterainc.com>
2014-06-11 11:51:28 -0700

    bcache: fix lost wakeup when shutting down GC thread
    This was a regression from f54ce966c, exposed by 589382df.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/btree.c
+++ drivers/md/bcache/btree.c
@@ -1785,11 +1785,11 @@ static int bch_gc_thread(void *arg)
 			set_current_state(TASK_RUNNING);
 		spin_unlock(&c->gc_lock);
 
-		try_to_freeze();
-		schedule();
-
 		if (kthread_should_stop())
 			break;
+
+		try_to_freeze();
+		schedule();
 	}
 
 	return 0;

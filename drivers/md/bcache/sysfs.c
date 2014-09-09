abbb38e622e1fcdbc084ca4faefe09c1fb65f452
Kent Overstreet <kmo@daterainc.com>
2014-09-08 23:15:23 -0700

    bcache: split up bucket_lock
    Change-Id: I1d077ccb86d6f9eab8b34b79276aba40fd4a916f

--- drivers/md/bcache/sysfs.c
+++ drivers/md/bcache/sysfs.c
@@ -902,7 +902,6 @@ static ssize_t show_quantiles(struct cache *ca, char *buf,
 	int cmp(const void *l, const void *r)
 	{	return *((unsigned *) r) - *((unsigned *) l); }
 
-	struct cache_set *c = ca->set;
 	size_t n = ca->sb.nbuckets, i;
 	/* Compute 31 quantiles */
 	unsigned q[31], *p;
@@ -912,10 +911,8 @@ static ssize_t show_quantiles(struct cache *ca, char *buf,
 	if (!p)
 		return -ENOMEM;
 
-	mutex_lock(&c->bucket_lock);
 	for (i = ca->sb.first_bucket; i < n; i++)
 		p[i] = fn(ca, &ca->buckets[i], private);
-	mutex_unlock(&c->bucket_lock);
 
 	sort(p, n, sizeof(unsigned), cmp, NULL);
 
@@ -942,7 +939,7 @@ static ssize_t show_reserve_stats(struct cache *ca, char *buf)
 	enum alloc_reserve i;
 	ssize_t ret;
 
-	mutex_lock(&ca->set->bucket_lock);
+	spin_lock(&ca->freelist_lock);
 
 	ret = scnprintf(buf, PAGE_SIZE,
 			"free_inc:\t%zu\t%zu\n",
@@ -955,7 +952,7 @@ static ssize_t show_reserve_stats(struct cache *ca, char *buf)
 				 fifo_used(&ca->free[i]),
 				 ca->free[i].size);
 
-	mutex_unlock(&ca->set->bucket_lock);
+	spin_unlock(&ca->freelist_lock);
 
 	return ret;
 }
@@ -1040,10 +1037,7 @@ STORE(__bch_cache)
 			return v;
 
 		if ((unsigned) v != CACHE_REPLACEMENT(mi)) {
-			mutex_lock(&c->bucket_lock);
 			SET_CACHE_REPLACEMENT(mi, v);
-			mutex_unlock(&c->bucket_lock);
-
 			bcache_write_super(c);
 		}
 	}
@@ -1058,7 +1052,6 @@ STORE(__bch_cache)
 			unsigned i;
 			struct cache_tier *tier;
 
-			mutex_lock(&c->bucket_lock);
 			tier = &c->cache_by_alloc[CACHE_TIER(mi)];
 
 			for (i = 0; i < tier->nr_devices; i++)
@@ -1075,8 +1068,6 @@ found:
 
 			tier = &c->cache_by_alloc[v];
 			tier->devices[tier->nr_devices++] = ca;
-
-			mutex_unlock(&c->bucket_lock);
 
 			SET_CACHE_TIER(mi, v);
 			bcache_write_super(c);

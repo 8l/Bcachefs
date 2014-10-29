a4b098cf52f8ac932cc22ccb8faae1bcdd9f9d76
Kent Overstreet <kmo@daterainc.com>
2014-10-29 14:49:56 -0700

    bcache: protect cache_member_info with rcu
    Change-Id: I195c06f0537d76ad167439708e6cb3b860dfb264

--- drivers/md/bcache/bcache.h
+++ drivers/md/bcache/bcache.h
@@ -219,6 +219,12 @@ struct cache {
 	struct cache_set	*set;
 	struct cache_sb		sb;
 
+	/*
+	 * Cached version of this device's member info from superblock
+	 * Committed by write_super()
+	 */
+	struct cache_member	mi;
+
 	struct bcache_superblock disk_sb;
 
 	struct kobject		kobj;
@@ -350,6 +356,12 @@ struct prio_clock {
 	unsigned __percpu	*rescale_percpu;
 };
 
+struct cache_member_rcu {
+	struct rcu_head		rcu;
+	unsigned		nr_in_set;
+	struct cache_member	m[];
+};
+
 struct cache_set {
 	struct closure		cl;
 	int			minor;
@@ -360,7 +372,7 @@ struct cache_set {
 	unsigned long		flags;
 
 	struct cache __rcu	*cache[MAX_CACHES_PER_SET];
-	struct cache_member	*members;
+	struct cache_member_rcu	*members;
 	unsigned long		cache_slots_used[BITS_TO_LONGS(MAX_CACHES_PER_SET)];
 
 	struct cache_sb		sb;

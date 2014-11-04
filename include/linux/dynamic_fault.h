bd1e38e7e04f4e690d2db60d0621527f9af01e7e
Kent Overstreet <kmo@daterainc.com>
2014-11-04 15:20:14 -0800

    dynamic faults: classes, frequencies, new parsing
    Change-Id: Id0d6e748e220d7550b4da61356d5a33a5036ab0f

--- include/linux/dynamic_fault.h
+++ include/linux/dynamic_fault.h
@@ -5,6 +5,23 @@
 #include <linux/jump_label.h>
 #include <linux/slab.h>
 
+enum dfault_enabled {
+	DFAULT_DISABLED,
+	DFAULT_ENABLED,
+	DFAULT_ONESHOT,
+};
+
+union dfault_state {
+	struct {
+		unsigned		enabled:2;
+		unsigned		count:30;
+	};
+
+	struct {
+		unsigned		v;
+	};
+};
+
 /*
  * An instance of this structure is created in a special
  * ELF section at every dynamic fault callsite.  At runtime,
@@ -14,39 +31,25 @@ struct _dfault {
 	const char		*modname;
 	const char		*function;
 	const char		*filename;
-	unsigned int		lineno:16;
-	unsigned int		index:16;
-	/*
-	 * The flags field controls the behaviour at the callsite.
-	 * The bits here are changed dynamically when the user
-	 * writes commands to <debugfs>/dynamic_debug/ddebug
-	 */
-#define _DFAULT_ON		(1<<0)
-#define _DFAULT_ONE_SHOT	(1<<1)
-	unsigned int		flags:8;
+	const char		*class;
+
+	const u16		lineno;
+	u16			index;
+
+	unsigned		frequency;
+	union dfault_state	state;
+
 	struct static_key	enabled;
 } __attribute__((aligned(8)));
 
 
 #ifdef CONFIG_DYNAMIC_FAULT
 
-extern long long dynamic_fault_enabled;
-extern long long dynamic_fault_enabled2;
-
 int dfault_add_module(struct _dfault *tab, unsigned int n, const char *mod);
 int dfault_remove_module(char *mod_name);
+bool __dynamic_fault_enabled(struct _dfault *);
 
-#define __dynamic_fault_enabled(df)					\
-({									\
-	int __ret = 0;							\
-	if (static_key_false(&(df).enabled)) {				\
-		__ret = df.flags;					\
-		df.flags &= ~_DFAULT_ONE_SHOT;				\
-	}								\
-	__ret;								\
-})
-
-#define dynamic_fault()							\
+#define dynamic_fault(_class)						\
 ({									\
 	static struct _dfault descriptor				\
 	__used __attribute__((section("__faults"), aligned(8))) = {	\
@@ -54,58 +57,52 @@ int dfault_remove_module(char *mod_name);
 		.function	= __func__,				\
 		.filename	= __FILE__,				\
 		.lineno		= __LINE__,				\
+		.class		= _class,				\
 	};								\
-	__dynamic_fault_enabled(descriptor);				\
+									\
+	static_key_false(&descriptor.enabled) &&			\
+		__dynamic_fault_enabled(&descriptor);			\
 })
 
-#define named_fault(name)						\
-({									\
-	static struct _dfault descriptor				\
-	__used __attribute__((section("__faults"), aligned(8))) = {	\
-		.modname	= KBUILD_MODNAME,			\
-		.function	= #name,				\
-		.filename	= __FILE__,				\
-		.lineno		= __LINE__,				\
-	};								\
-	__dynamic_fault_enabled(descriptor);				\
-})
+#define memory_fault()		dynamic_fault("memory")
+#define race_fault()		dynamic_fault("race")
 
 #define kmalloc(...)							\
-	(dynamic_fault() ? NULL		: kmalloc(__VA_ARGS__))
+	(memory_fault() ? NULL	: kmalloc(__VA_ARGS__))
 #define kzalloc(...)							\
-	(dynamic_fault() ? NULL		: kzalloc(__VA_ARGS__))
+	(memory_fault() ? NULL	: kzalloc(__VA_ARGS__))
 #define krealloc(...)							\
-	(dynamic_fault() ? NULL		: krealloc(__VA_ARGS__))
+	(memory_fault() ? NULL	: krealloc(__VA_ARGS__))
 
 #define __get_free_pages(...)						\
-	(dynamic_fault() ? 0		: __get_free_pages(__VA_ARGS__))
+	(memory_fault() ? 0	: __get_free_pages(__VA_ARGS__))
 #define alloc_pages_node(...)						\
-	(dynamic_fault() ? NULL		: alloc_pages_node(__VA_ARGS__))
+	(memory_fault() ? NULL	: alloc_pages_node(__VA_ARGS__))
 #define alloc_pages_nodemask(...)					\
-	(dynamic_fault() ? NULL		: alloc_pages_nodemask(__VA_ARGS__))
+	(memory_fault() ? NULL	: alloc_pages_nodemask(__VA_ARGS__))
 
 #define bio_alloc_bioset(gfp, ...)					\
-	(!(gfp & __GFP_WAIT) && dynamic_fault()				\
+	(!(gfp & __GFP_WAIT) && memory_fault()				\
 	 ? NULL	: bio_alloc_bioset(gfp, __VA_ARGS__))
 
 #define bio_clone(bio, gfp)						\
-	(!(gfp & __GFP_WAIT) && dynamic_fault()				\
+	(!(gfp & __GFP_WAIT) && memory_fault()				\
 	 ? NULL	: bio_clone(bio, gfp))
 
 #define bio_clone_bioset(bio, gfp, bs)					\
-	(!(gfp & __GFP_WAIT) && dynamic_fault()				\
+	(!(gfp & __GFP_WAIT) && memory_fault()				\
 	 ? NULL	: bio_clone_bioset(bio, gfp, bs))
 
 #define bio_kmalloc(...)						\
-	(dynamic_fault() ? NULL		: bio_kmalloc(__VA_ARGS__))
+	(memory_fault() ? NULL		: bio_kmalloc(__VA_ARGS__))
 #define bio_clone_kmalloc(...)						\
-	(dynamic_fault() ? NULL		: bio_clone_kmalloc(__VA_ARGS__))
+	(memory_fault() ? NULL		: bio_clone_kmalloc(__VA_ARGS__))
 #define bio_alloc_pages(...)						\
-	(dynamic_fault() ? -ENOMEM	: bio_alloc_pages(__VA_ARGS__))
+	(memory_fault() ? -ENOMEM	: bio_alloc_pages(__VA_ARGS__))
 
 #define bio_get_user_pages(bio, uaddr, len, write_to_vm)		\
 	bio_get_user_pages(bio, uaddr,					\
-		dynamic_fault() ? min_t(unsigned long, len, PAGE_SIZE)	\
+		memory_fault() ? min_t(unsigned long, len, PAGE_SIZE)	\
 				: len,					\
 		write_to_vm)
 
@@ -113,8 +110,9 @@ int dfault_remove_module(char *mod_name);
 
 #define dfault_add_module(tab, n, modname)	0
 #define dfault_remove_module(mod)		0
-#define dynamic_fault()				0
-#define named_fault(name)			0
+#define dynamic_fault(_class)			0
+#define memory_fault()				0
+#define race_fault()				0
 
 #endif /* CONFIG_DYNAMIC_FAULT */
 

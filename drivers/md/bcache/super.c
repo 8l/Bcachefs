4604123bcce9973e51def7d06b6f822d65ebeeec
Slava Pestov <sp@daterainc.com>
2014-11-28 16:51:10 -0800

    bcache: add tracepoints for cache set and cache going read only
    This is intended to help debug another issue.
    
    Change-Id: I2de4a31a37751e59d67b890cf695b204f48f59ca

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -612,6 +612,8 @@ static void bch_cache_set_read_only(struct cache_set *c)
 	if (test_and_set_bit(CACHE_SET_RO, &c->flags))
 		return;
 
+	trace_bcache_cache_set_read_only(c);
+
 	bch_wake_delayed_writes((unsigned long) c);
 	del_timer_sync(&c->foreground_write_wakeup);
 	cancel_delayed_work_sync(&c->pd_controllers_update);
@@ -638,6 +640,8 @@ static void bch_cache_set_read_only(struct cache_set *c)
 		/* flush last journal entry if needed */
 		c->journal.work.work.func(&c->journal.work.work);
 	}
+
+	trace_bcache_cache_set_read_only_done(c);
 }
 
 void bch_cache_set_fail(struct cache_set *c)
@@ -1316,6 +1320,8 @@ void bch_cache_read_only(struct cache *ca)
 
 	cache_member_info_put();
 
+	trace_bcache_cache_read_only(ca);
+
 	bch_moving_gc_stop(ca);
 
 	/*
@@ -1348,6 +1354,8 @@ void bch_cache_read_only(struct cache *ca)
 	 * after this point.
 	 */
 
+	trace_bcache_cache_read_only_done(ca);
+
 	pr_notice("%s read only", bdevname(ca->bdev, buf));
 }
 
@@ -1361,6 +1369,8 @@ const char *bch_cache_read_write(struct cache *ca)
 
 	cache_member_info_put();
 
+	trace_bcache_cache_read_write(ca);
+
 	err = bch_cache_allocator_start(ca);
 	if (err)
 		return err;
@@ -1369,6 +1379,8 @@ const char *bch_cache_read_write(struct cache *ca)
 	bch_cache_group_add_cache(&c->cache_all, ca);
 
 	bch_recalc_capacity(c);
+
+	trace_bcache_cache_read_write_done(ca);
 
 	return NULL;
 }

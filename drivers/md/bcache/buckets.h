d80a0fc91d04183fe779a0ed32356c7aebe63094
Kent Overstreet <kmo@daterainc.com>
2014-12-06 16:45:44 -0800

    bcache: assertions for available -> unavailable bucket state transition
    Change-Id: I3f8a5229725bc18e73a4f4be27a824fb58464d9a

--- drivers/md/bcache/buckets.h
+++ drivers/md/bcache/buckets.h
@@ -237,7 +237,7 @@ static inline bool is_available_bucket(struct bucket_mark mark)
 
 void bch_mark_free_bucket(struct cache *, struct bucket *);
 void bch_mark_alloc_bucket(struct cache *, struct bucket *);
-void bch_mark_metadata_bucket(struct cache *, struct bucket *);
+void bch_mark_metadata_bucket(struct cache *, struct bucket *, bool);
 uint8_t bch_mark_data_bucket(struct cache_set *, struct cache *,
 			     struct bkey *, unsigned, int,
 			     bool, bool);

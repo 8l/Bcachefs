fab9ee78d8000e4981d8e63adbb10e14fe17133e
Slava Pestov <sp@daterainc.com>
2014-12-15 10:52:57 -0800

    bcache: fix tiering writequeue start when adding device at runtime
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1110,6 +1110,12 @@ static const char *run_cache_set(struct cache_set *c)
 			percpu_ref_put(&ca->ref);
 			goto err;
 		}
+
+		err = "error starting tiering write workqueue";
+		if (bch_tiering_write_start(ca)) {
+			percpu_ref_put(&ca->ref);
+			goto err;
+		}
 	}
 
 	err = "error starting tiering thread";
@@ -1430,6 +1436,10 @@ const char *bch_cache_read_write(struct cache *ca)
 
 	err = "error starting moving GC thread";
 	if (!bch_moving_gc_thread_start(ca))
+		err = NULL;
+
+	err = "error starting tiering write workqueue";
+	if (!bch_tiering_write_start(ca))
 		err = NULL;
 
 	return err;

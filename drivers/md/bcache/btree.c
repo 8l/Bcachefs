58d9276f09c8d221fc902884681440b626ac9f8a
Kent Overstreet <kmo@daterainc.com>
2015-01-03 16:32:21 -0800

    bcache: Fix a journalling bug
    journal_res_put() would always set the journal to dirty, but this was incorrect
    - we sometimes have to put a journal reservation that we didn't do anything
    with, e.g. if the btree node immediately needed to be split.
    
    Fix this by setting the journal dirty in bch_journal_add_keys() instead.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/btree.c
+++ drivers/md/bcache/btree.c
@@ -1319,7 +1319,8 @@ static void bch_btree_set_root(struct btree *b)
 	bch_recalc_btree_reserve(c);
 
 	if (old) {
-		bch_journal_res_put(c, &res, &cl);
+		bch_journal_set_dirty(c, &cl);
+		bch_journal_res_put(c, &res);
 		closure_sync(&cl);
 
 		six_unlock_write(&old->lock);
@@ -1731,9 +1732,7 @@ bch_btree_insert_keys(struct btree *b,
 		six_unlock_write(&b->lock);
 
 		if (res.ref)
-			bch_journal_res_put(iter->c, &res,
-					    bch_keylist_empty(insert_keys)
-					    ? persistent : NULL);
+			bch_journal_res_put(iter->c, &res);
 	}
 
 	if (inserted && b->written) {

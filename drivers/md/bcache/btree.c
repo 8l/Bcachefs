4e82d7cf2252d822075bf7ec30c17fd159cd2b71
Slava Pestov <sp@daterainc.com>
2015-01-05 01:04:25 -0800

    bcache: try to write multiple btree nodes when journal is full
    When we are out of journal buckets: instead of only writing out
    the oldest node, write out enough nodes to free up a journal bucket
    on each journal device.
    
    When the pin fifo is full: write out enough nodes to free up 1/16th
    of the FIFO.
    
    Finally, increase the journal.pin FIFO size to match the max journal
    size of 512MB. In the future, this FIFO should be made resizable.
    
    Also add some comments attempting to clarify how the journal code
    works.
    
    Change-Id: I4d14c5b204850d1c21d69026b6c1b960f038d93d

--- drivers/md/bcache/btree.c
+++ drivers/md/bcache/btree.c
@@ -605,41 +605,41 @@ static void btree_node_write_work(struct work_struct *w)
 	bch_btree_node_write_dirty(b, NULL);
 }
 
-void bch_btree_write_oldest(struct cache_set *c)
+/**
+ * bch_btree_write_oldest - write all btree nodes with sequence numbers older
+ * than @oldest_seq
+ */
+void bch_btree_write_oldest(struct cache_set *c, u64 oldest_seq)
 {
 	/*
 	 * Try to find the btree node with that references the oldest journal
 	 * entry, best is our current candidate and is locked if non NULL:
 	 */
-	struct btree *b, *best;
+	struct btree *b;
 	unsigned i;
-retry:
-	cond_resched();
-	best = NULL;
+	int written = 0;
+	struct closure cl;
+
+	closure_init_stack(&cl);
+
+	trace_bcache_journal_write_oldest(c, oldest_seq);
 
 	for_each_cached_btree(b, c, i)
 		if (btree_current_write(b)->journal) {
-			if (!best)
-				best = b;
-			else if (journal_pin_cmp(c,
-					btree_current_write(best)->journal,
-					btree_current_write(b)->journal)) {
-				best = b;
+			if (fifo_idx(&c->journal.pin,
+				     btree_current_write(b)->journal)
+				<= oldest_seq) {
+				six_lock_read(&b->lock);
+				if (btree_current_write(b)->journal) {
+					written++;
+					__bch_btree_node_write(b, &cl);
+				}
+				six_unlock_read(&b->lock);
 			}
 		}
 
-	b = best;
-	if (b) {
-		six_lock_read(&b->lock);
-		if (!btree_current_write(b)->journal) {
-			six_unlock_read(&b->lock);
-			/* We raced */
-			goto retry;
-		}
-
-		__bch_btree_node_write(b, NULL);
-		six_unlock_read(&b->lock);
-	}
+	closure_sync(&cl);
+	trace_bcache_journal_write_oldest_done(c, oldest_seq, written);
 }
 
 /*

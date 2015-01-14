ec4157d2c8c086ae2a851e762058e9fe4b52403f
Slava Pestov <sp@daterainc.com>
2015-01-13 18:19:44 -0800

    bcache: add read_issued flag to struct moving_io
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/move.c
+++ drivers/md/bcache/move.c
@@ -96,7 +96,7 @@ static void moving_io_destructor(struct closure *cl)
 	BUG_ON(!q->count);
 	q->count--;
 
-	if (!io->read_completed) {
+	if (io->read_issued) {
 		BUG_ON(!q->read_count);
 		q->read_count--;
 	}
@@ -360,7 +360,9 @@ static void read_moving_endio(struct bio *bio, int error)
 
 	trace_bcache_move_read_done(q, &io->key);
 
+	BUG_ON(!io->read_issued);
 	BUG_ON(io->read_completed);
+	io->read_issued = 0;
 	io->read_completed = 1;
 	BUG_ON(!q->read_count);
 	q->read_count--;
@@ -434,6 +436,8 @@ void bch_data_move(struct moving_queue *q,
 	if (q->stopped)
 		stopped = true;
 	else {
+		BUG_ON(io->read_issued);
+		io->read_issued = 1;
 		q->count++;
 		q->read_count++;
 		list_add_tail(&io->list, &q->pending);

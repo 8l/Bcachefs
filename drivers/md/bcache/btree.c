0a28d42d185f2af16d514a3ac7f629dcb2040b54
Slava Pestov <sp@daterainc.com>
2015-01-14 17:59:56 -0800

    bcache: add mca_alloc_time and mca_scan_time to sysfs
    Change-Id: Id90af86eb6a1765d4fb8eabd9f2d86a2f804c80a

--- drivers/md/bcache/btree.c
+++ drivers/md/bcache/btree.c
@@ -787,6 +787,8 @@ static unsigned long bch_mca_scan(struct shrinker *shrink,
 	unsigned long i, nr = sc->nr_to_scan;
 	unsigned long freed = 0;
 
+	u64 start_time = local_clock();
+
 	if (c->shrinker_disabled)
 		return SHRINK_STOP;
 
@@ -843,6 +845,9 @@ static unsigned long bch_mca_scan(struct shrinker *shrink,
 	}
 
 	mutex_unlock(&c->btree_cache_lock);
+
+	bch_time_stats_update(&c->mca_scan_time, start_time);
+
 	return freed * c->btree_pages;
 }
 
@@ -1041,6 +1046,8 @@ static struct btree *mca_alloc(struct cache_set *c, const struct bkey *k,
 {
 	struct btree *b = NULL;
 
+	u64 start_time = local_clock();
+
 	mutex_lock(&c->btree_cache_lock);
 
 	if (mca_find(c, k))
@@ -1093,6 +1100,9 @@ out:
 
 out_unlock:
 	mutex_unlock(&c->btree_cache_lock);
+
+	bch_time_stats_update(&c->mca_alloc_time, start_time);
+
 	return b;
 err:
 	if (b) {

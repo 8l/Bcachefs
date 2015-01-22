d469ef8328c141b0a725eeb115fbd67c257cc101
Slava Pestov <sp@daterainc.com>
2015-01-22 00:40:19 -0800

    bcache: wake up tiering thread after device goes read write
    If this is the first tier 1 device, tiering will be sleeping waiting
    for the device count to change.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1089,12 +1089,6 @@ static const char *run_cache_set(struct cache_set *c)
 			percpu_ref_put(&ca->ref);
 			goto err;
 		}
-
-		err = "error starting tiering write workqueue";
-		if (bch_tiering_write_start(ca)) {
-			percpu_ref_put(&ca->ref);
-			goto err;
-		}
 	}
 
 	err = "error starting tiering thread";
@@ -1417,9 +1411,7 @@ const char *bch_cache_read_write(struct cache *ca)
 	if (!bch_moving_gc_thread_start(ca))
 		err = NULL;
 
-	err = "error starting tiering write workqueue";
-	if (!bch_tiering_write_start(ca))
-		err = NULL;
+	wake_up_process(ca->set->tiering_read);
 
 	return err;
 }

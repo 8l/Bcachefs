0d589a8d2543b97cf49fe5ecb0dfe76e9eff2116
Kent Overstreet <kent.overstreet@gmail.com>
2015-05-04 17:15:48 -0700

    bcache: Convert to inline mempools/biosets

--- drivers/md/bcache/bset.c
+++ drivers/md/bcache/bset.c
@@ -1428,8 +1428,7 @@ EXPORT_SYMBOL(bch_btree_node_iter_next_unpack);
 
 void bch_bset_sort_state_free(struct bset_sort_state *state)
 {
-	if (state->pool)
-		mempool_destroy(state->pool);
+	mempool_exit(&state->pool);
 }
 
 int bch_bset_sort_state_init(struct bset_sort_state *state, unsigned page_order)
@@ -1439,8 +1438,7 @@ int bch_bset_sort_state_init(struct bset_sort_state *state, unsigned page_order)
 	state->page_order = page_order;
 	state->crit_factor = int_sqrt(1 << page_order);
 
-	state->pool = mempool_create_page_pool(1, page_order);
-	if (!state->pool)
+	if (mempool_init_page_pool(&state->pool, 1, page_order))
 		return -ENOMEM;
 
 	return 0;

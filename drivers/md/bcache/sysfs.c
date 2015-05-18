084336e260e07d9e89edeb29bec13b36260aef88
Kent Overstreet <kent.overstreet@gmail.com>
2015-05-17 19:33:10 -0700

    bcache: CACHE_PREFERRED_CSUM_TYPE -> CACHE_META_PREFERRED_CSUM_TYPE

--- drivers/md/bcache/sysfs.c
+++ drivers/md/bcache/sysfs.c
@@ -633,7 +633,7 @@ SHOW(bch_cache_set)
 	if (attr == &sysfs_checksum_type)
 		return bch_snprint_string_list(buf, PAGE_SIZE,
 				bch_csum_types,
-				CACHE_PREFERRED_CSUM_TYPE(&c->sb));
+				CACHE_META_PREFERRED_CSUM_TYPE(&c->sb));
 
 	if (attr == &sysfs_errors)
 		return bch_snprint_string_list(buf, PAGE_SIZE, error_actions,
@@ -847,8 +847,8 @@ STORE(__bch_cache_set)
 		if (v < 0)
 			return v;
 
-		if (v != CACHE_PREFERRED_CSUM_TYPE(&c->sb)) {
-			SET_CACHE_PREFERRED_CSUM_TYPE(&c->sb, v);
+		if (v != CACHE_META_PREFERRED_CSUM_TYPE(&c->sb)) {
+			SET_CACHE_META_PREFERRED_CSUM_TYPE(&c->sb, v);
 			bcache_write_super(c);
 		}
 

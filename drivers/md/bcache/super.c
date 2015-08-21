7404d82f8403cfeb1f795876ee0fb3653655add3
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-20 23:47:52 -0800

    bcache: Add refcount to open_buckets
    Prep work for finally getting rid of bucket->pin!
    
    The way we're gonig to get rid of bucket pin is by having buckets owned by
    something that the garbage collector can find until after we've inserted the new
    keys that point to that bucket into the btree.
    
    This adds code for allocating struct open_buckets, and freeing them when their
    refcount goes to 0, and reworks bch_alloc_sectors() for the new way of
    allocating buckets and to pass the pointer to the struct open_bucket back to
    bch_data_insert_start().
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1329,7 +1329,6 @@ static void cache_set_free(struct closure *cl)
 	if (!IS_ERR_OR_NULL(c->debug))
 		debugfs_remove(c->debug);
 
-	bch_open_buckets_free(c);
 	bch_btree_cache_free(c);
 	bch_journal_free(c);
 
@@ -1495,6 +1494,7 @@ struct cache_set *bch_cache_set_alloc(struct cache_sb *sb)
 	spin_lock_init(&c->btree_split_time.lock);
 	spin_lock_init(&c->btree_read_time.lock);
 
+	bch_open_buckets_init(c);
 	bch_moving_init_cache_set(c);
 
 	INIT_LIST_HEAD(&c->list);
@@ -1502,7 +1502,6 @@ struct cache_set *bch_cache_set_alloc(struct cache_sb *sb)
 	INIT_LIST_HEAD(&c->btree_cache);
 	INIT_LIST_HEAD(&c->btree_cache_freeable);
 	INIT_LIST_HEAD(&c->btree_cache_freed);
-	INIT_LIST_HEAD(&c->data_buckets);
 
 	INIT_WORK(&c->bio_submit_work, bch_bio_submit_work);
 	spin_lock_init(&c->bio_submit_lock);
@@ -1525,7 +1524,6 @@ struct cache_set *bch_cache_set_alloc(struct cache_sb *sb)
 	    !(c->btree_insert_wq = create_workqueue("bcache_btree")) ||
 	    bch_journal_alloc(c) ||
 	    bch_btree_cache_alloc(c) ||
-	    bch_open_buckets_alloc(c) ||
 	    bch_bset_sort_state_init(&c->sort, ilog2(c->btree_pages)))
 		goto err;
 
